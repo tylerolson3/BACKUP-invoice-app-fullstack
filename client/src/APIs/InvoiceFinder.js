40fd4b386ba49742f4374e8841d4d28cc0122c2d
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-02-18 13:21:23 -0600

    updated axios baseUrl

--- client/src/APIs/InvoiceFinder.js
+++ client/src/APIs/InvoiceFinder.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3001/api/v1/invoices",
+  // baseURL: "http://localhost:3001/api/v1/invoices",
+  baseURL: "https://invoice-app-tyler.herokuapp.com/api/v1/invoices",
 });
