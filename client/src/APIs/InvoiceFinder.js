7b5333471043c671d727a61547841ab2b773af54
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-02-18 13:41:40 -0600

    add axios retry

--- client/src/APIs/InvoiceFinder.js
+++ client/src/APIs/InvoiceFinder.js
@@ -1,6 +1,19 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
 
-export default axios.create({
-  // baseURL: "http://localhost:3001/api/v1/invoices",
-  baseURL: "https://invoice-app-tyler.herokuapp.com/api/v1/invoices",
+const InvoiceFinder = axios.create({
+  baseURL:
+    process.env.NODE_ENV !== "production"
+      ? "https://localhost:6000/api/v1/invoices"
+      : "https://invoice-app-tyler.herokuapp.com/api/v1/invoices",
+  timeout: 5000,
 });
+
+axiosRetry(InvoiceFinder, { retries: 3 });
+
+export default InvoiceFinder;
+
+// export default axios.create({
+//   // baseURL: "http://localhost:3001/api/v1/invoices",
+//   baseURL: "https://invoice-app-tyler.herokuapp.com/api/v1/invoices",
+// });
